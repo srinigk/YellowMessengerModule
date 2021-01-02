e0523aea3b34180f7b80d0a5fd75cb22d55a5c5b
G Srinivasa <srinivasa.g@connectm.com>
2021-01-02 07:58:59 +0530

    Initial Commit

---
+++ YellowMessengerModule/YellowMessengerModule.h
@@ -0,0 +1,19 @@
+//
+//  YellowMessengerModule.h
+//  YellowMessengerModule
+//
+//  Created by G Srinivasa on 02/01/21.
+//  Copyright © 2021 G Srinivasa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YellowMessengerModule.
+FOUNDATION_EXPORT double YellowMessengerModuleVersionNumber;
+
+//! Project version string for YellowMessengerModule.
+FOUNDATION_EXPORT const unsigned char YellowMessengerModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YellowMessengerModule/PublicHeader.h>
+
+
---
+++ YellowMessengerModuleTests/YellowMessengerModuleTests.swift
@@ -0,0 +1,34 @@
+//
+//  YellowMessengerModuleTests.swift
+//  YellowMessengerModuleTests
+//
+//  Created by G Srinivasa on 02/01/21.
+//  Copyright © 2021 G Srinivasa. All rights reserved.
+//
+
+import XCTest
+@testable import YellowMessengerModule
+
+class YellowMessengerModuleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
